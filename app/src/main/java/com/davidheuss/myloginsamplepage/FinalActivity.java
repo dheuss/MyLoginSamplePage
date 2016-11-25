72ae327fe180579ae80fa38711728fbd37d9c833
David Heuss <heussd@cip.ifi.lmu.de>
2016-11-25 16:57:09 +0100

    update

---
+++ app/src/main/java/com/davidheuss/myloginsamplepage/FinalActivity.java
@@ -0,0 +1,17 @@
+package com.davidheuss.myloginsamplepage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by david on 25.11.2016.
+ */
+
+public class FinalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final);
+    }
+}
