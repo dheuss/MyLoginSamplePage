d616c0f75b7748021fcadf069043eda0bf8f8ecc
David Heuss <heussd@cip.ifi.lmu.de>
2016-11-25 16:29:38 +0100

    init

---
+++ app/src/main/java/com/davidheuss/myloginsamplepage/GoogleLoginActivity.java
@@ -0,0 +1,17 @@
+package com.davidheuss.myloginsamplepage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by david on 25.11.2016.
+ */
+
+public class GoogleLoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_login);
+    }
+}
---
+++ app/src/main/java/com/davidheuss/myloginsamplepage/MainActivity.java
@@ -0,0 +1,51 @@
+package com.davidheuss.myloginsamplepage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.FacebookActivity;
+
+/**
+ * Created by david on 25.11.2016.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_facebook;
+    private Button btn_google;
+    private Button btn_email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_facebook = (Button) findViewById(R.id.btn_login_facebook);
+        btn_google = (Button) findViewById(R.id.btn_login_google);
+        btn_email = (Button) findViewById(R.id.btn_login_email);
+
+        btn_facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                startActivity(new Intent(MainActivity.this, FacebookLoginActivity.class));
+            }
+        });
+
+        btn_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GoogleLoginActivity.class));
+            }
+        });
+
+        btn_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmailLoginActivity.class));
+            }
+        });
+    }
+}
